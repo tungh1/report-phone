4649f5286438105fb19da2c1b899c6a2866a3a89
Tung Hoang <tunghoang2011@gmail.com>
2023-05-12 01:01:26 +0700

    f

--- web.js
+++ web.js
@@ -2,14 +2,15 @@ const express=require('express');
 var router = express.Router();
 const path = require('path');
 
+router.get('/js',function(req,res){
+    res.sendFile(path.join(__dirname+'/www/js/index.js'));
+    //__dirname : It will resolve to your project folder.
+});
+
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'/www/index.html'));
     //__dirname : It will resolve to your project folder.
 });
 
-router.get('/js/index.js',function(req,res){
-    res.sendFile(path.join(__dirname+'/www/js/index.js'));
-    //__dirname : It will resolve to your project folder.
-});
 
 module.exports = router;
